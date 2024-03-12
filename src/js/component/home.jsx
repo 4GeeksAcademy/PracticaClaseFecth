3c271cd4646a686f70124500488d4c9b63929b6a
clisdermar <clisdermar@gmail.com>
2024-03-12 18:15:22 +0000

    Practica Fetch Naruto en clase

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [charactersNumber,setCharactersNumber]=useState(0)
+	const [page,setPage] = useState(1)
+
+	useEffect(()=>{
+		//GET
+		fetch(`https://narutodb.xyz/api/character?page=${page}&limit=5`)
+		.then(resp => 
+		{
+	
+			return resp.json() //Response ,capturo el Json y Luego lo convierto en data
+		})
+		.then(data =>{
+			console.log(data) //objeto JS
+			setNarutoCharacters(data.characters)
+			setCharactersNumber(data.totalCharacters)
+
+		})
+		.catch(error => {
+			console.log(error);
+		})
+
+	},[page])
+
+const [narutoCharacters, setNarutoCharacters] = useState([])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center aling-items-center text-center min-vh-100">
+			<h1>Characteres {charactersNumber}!</h1>
+			<button onClick={()=> setPage(page+ 1)}>Nex</button>
+			{
+				narutoCharacters.map((person)=> <div key = {person.id}>
+					
+					<img src={person.images[0]} alt={person.name} style={{width: '180px'}} />
+					<p>{person.name}</p>
+					
+					</div>)
+			}
 		</div>
 	);
 };
